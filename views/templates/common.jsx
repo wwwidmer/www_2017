8680c270f0e5427c572e1f4b56a6f6da405a0102
wwwidmer <williamwidmerr@gmail.com>
2017-10-03 11:42:01 -0400

    typos

--- views/templates/common.jsx
+++ views/templates/common.jsx
@@ -2,12 +2,11 @@ class BlogShowCase extends React.Component {
       render() {
       	       return (
 	       	      <div>
-			<h3> {this.props.latest_blog_entry.title} | {this.props.latest_blog_entry.entry_date </h3>
+			<h3> {this.props.latest_blog_entry.title} | {this.props.latest_blog_entry.entry_date} </h3>
 			<img src={this.props.latest_blog_entry.image_url} alt=""/>
 			<p>{this.props.latest_blog_entry.description}</p>
 		      </div>
 		)
       }
-
 }
 
