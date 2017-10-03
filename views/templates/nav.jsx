4c41805e8010d1ce6f40bdea3095acc3570f2e3f
wwwidmer <williamwidmerr@gmail.com>
2017-10-02 20:23:08 -0400

    added templates, began react

---
+++ views/templates/nav.jsx
@@ -0,0 +1,26 @@
+class NavigationBar extends React.Component {
+      renderMenuItem(href, name){
+	return <MenuItem href={href}, name={name} />;
+      } 
+     render () {
+      	     return (
+	       <nav class="navbar navbar-default navbar-static-top navbar-inverse">				        <div class="container"	
+		    <ul class="nav navbar-nav">
+		    {this.renderMenuItem("/", "Home")}
+		    {this.renderMenuItem("/blog", "Blog")}
+		    </ul>
+		 </div>
+		</nav>	      
+	     )
+      }
+}
+
+class MenuItem extends React.Component {
+      constructor() {
+          super();
+ 	  this.state = {href:"/", name:"Home"}
+      }
+      render(){
+	<li href="{this.state.href}"> {this.state.name} </li>
+      }	    
+}
