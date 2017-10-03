887d7bc8c3dc1558d56f7dfabbb0ed62e6afbd84
wwwidmer <williamwidmerr@gmail.com>
2017-10-03 11:51:12 -0400

    script type

--- public/components/common.jsx
+++ public/components/common.jsx
@@ -3,7 +3,7 @@ class BlogShowCase extends React.Component {
       	       return (
 	       	      <div>
 			<h3> {this.props.latest_blog_entry.title} | {this.props.latest_blog_entry.entry_date} </h3>
-			<img src={this.props.latest_blog_entry.image_url} alt=""/>
+			<img src={this.props.latest_blog_entry.image_url} alt="" />
 			<p>{this.props.latest_blog_entry.description}</p>
 		      </div>
 		)
