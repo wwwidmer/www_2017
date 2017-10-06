fc558b9e113d405f1c7ed1265732792e78634110
wwwidmer <williamwidmerr@gmail.com>
2017-10-05 21:39:48 -0400

    actually get correct values

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
  });
+
+app.use(express.static(__dirname + '/public'));
+
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
