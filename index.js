c7bf17d87df4a4d1a9764669578ace1bdb3ab4e1
wwwidmer <williamwidmerr@gmail.com>
2017-10-09 15:36:01 -0400

    saving work, added post detail but not quite done. also prod

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.set('prod', (process.env.PROD || false));
+
+app.locals.RBP = {
+	api_prefix: app.get('prod') ? 'https://agile-earth-31601.herokuapp.com/': 'http://127.0.0.1:8000/'
+};
+app.locals.test = 'TEST';
 
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -24,6 +30,10 @@ app.get('/blog/', function(request, response) {
     response.render('pages/blog');
 });
 
+app.get('/blog/:postid', function(request, response) {
+    response.render('pages/post_detail', {post_id: 1})
+})
+
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Node (production:', !!(app.get('prod')),') app is running on port', app.get('port'));
 });
--- public/components/common.jsx
+++ public/components/common.jsx
@@ -1,3 +1,12 @@
+$(function(){
+    $('.navbar-nav li a').each(function(){
+      if ($(this).prop('href') == window.location.href) {
+          $(this).parent().addClass('active');
+      }
+    });
+});
+
+
 function get_url_async(url) {
   return new Promise(function(resolve, reject){
         var req = new XMLHttpRequest();
@@ -20,14 +29,13 @@ function get_url_async(url) {
 
 class BlogShowCase extends React.Component {
       render() {
-            return (
-       	<div>
-      		<h3> {this.props.latest_blog_entry.title} | {this.props.latest_blog_entry.timestamp} </h3>
-      		<img src={this.props.latest_blog_entry.image_url} alt="" />
-		    <p>{this.props.latest_blog_entry.description}</p>
-	      </div>
-		)
-      }
+        return (
+         	<div>
+        		<a href="/blog"> <h3> {this.props.latest_blog_entry.title} | {this.props.latest_blog_entry.timestamp.split('T')[0]} </h3></a>
+        		<img src={this.props.latest_blog_entry.image_url} alt="" />
+  		    <p>{this.props.latest_blog_entry.text}</p>
+  	      </div>
+  		)}
 }
 
 
@@ -37,16 +45,23 @@ class BlogList extends React.Component {
       }
       render() {
             var blogposts = [];
-            for (var i = 0; i < this.props.blog_list.length; i++) {
+            for (var i=this.props.blog_list.length-1; i >= 0 ; i--) {
                 blogposts.push(<BlogPost key={i} post_detail={this.props.blog_list[i]}/>)
             }
-          return (<ul> {blogposts} </ul>)
+          return (<ul class="list-group"> {blogposts} </ul>)
       }
 }
 
 
 class BlogPost extends React.Component {
       render () {
-      	     return (<li> {this.props.post_detail.title} </li>)
+      	     return (
+                <a class="list-group-item" href={"/blog/" + this.props.post_detail.id}>
+                    <div class="d-flex w-100 justify-content-between">
+                    <h4> {this.props.post_detail.title} </h4>
+                    <small> {this.props.post_detail.timestamp.split('T')[0]} </small>
+                  </div>
+                <p class="mb-1">{this.props.post_detail.description}</p>
+                </a>)
       }
 }
